6ab9cb0fb4a8e6c9594a8ac1915a0eb5bec82692
jffjck-dev <jeanfrancoiso.oclock@gmail.com>
2023-07-11 00:41:38 +0200

    feat: create landing page and components + actions

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,24 @@
+import Container from "@/components/ui/container";
+import Billboard from "@/components/billboard";
+import ProductsList from "@/components/products-list";
+import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+    const billboard = await getBillboard("873917f6-acd0-4ffa-87ce-fc9fce9ed552");
+    const products = await getProducts({isFeatured: true});
+    return (
+        <Container>
+            <div className="space-y-10 pb-10">
+                <Billboard data={billboard}/>
+                <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+                    <ProductsList title="Featured Products" items={products}/>
+                </div>
+            </div>
+        </Container>
+    );
+}
+
+export default HomePage;
